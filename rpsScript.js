a176604adefad4d2a9d674f1be240b2d0afe5bd8
AlexUmha <199190112+AlexUmha@users.noreply.github.com>
2026-01-09 07:46:14 -0500

    Rewrite game's underlying code
    Code is now more in line with the prompt from the Odin Project.

--- rpsScript.js
+++ rpsScript.js
@@ -1,62 +1,92 @@
-let randNum = 0;
-let compRock = "Computer chose rock. ";
-let compScis = "Computer chose scissors. ";
-let compPaper = "Computer chose paper. ";
-let compTie = "It's a tie.";
-let compLose = "You lose, haha! What a loser!";
-let compWin = "You win. Good for you!";
-let humanScore = 0;
-let compScore = 0;
-
-function rpsGame(input) {
-    console.log("You chose " + input)
-    if (input === "rock") {
-        if (randNum === 0) {
-            console.log(compRock + compTie)
-        } else if (randNum === 1) {
-            console.log(compPaper + compLose)
-            compScore++;
-        } else {
-            console.log(compScis + compWin)
-            humanScore++;
-        }
-    } else if (input === "paper") {
-        if (randNum === 0) {
-            console.log (compRock + compWin)
-            humanScore++;
-        } else if (randNum === 1) {
-            console.log (compPaper + compTie)
-        } else {
-            console.log (compScis + compLose)
-            compScore++;
-        }
-    } else if (input === "scissors") {
-        if (randNum === 0) {
-            console.log (compRock + compLose)
-            compScore++;
-        } else if (randNum === 1) {
-            console.log (compPaper + compWin)
-            humanScore++;
-        } else {
-            console.log (compScis + compTie)
-        }
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*3)
+    if (computerChoice === 0) {
+        return "rock"
+    }   else if (computerChoice === 1) {
+        return "paper"
     } else {
-        console.log ("There are three choices, dingus, just choose one. Since you wanna be dumb, you can lose this round.");
-        compScore++;
+        return "scissors"
     }
 }
-for (let i = 0; i < 5; i++) {
-    randNum = Math.floor(Math.random()*3);
-    let input = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    rpsGame(input);
-    if (i === 4){
-        console.log(`Final results: Your Score: ${humanScore}. | Bot score: ${compScore}.`)
-        if (humanScore > compScore) {
-            console.log("Wow, you did it! You are the ultimate winner.")
-        } else if (humanScore < compScore) {
-            console.log("Haha, you're a loser! Look everybody, point and laugh at the loser!")
-        } else {
-            console.log("wow, a tie, how boring.")
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper, or scissors?")
+    return humanChoice
+}
+
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice();
+        switch (true) {
+            case (humanChoice == "rock" && computerChoice == "rock"):
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. Tie. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "rock" && computerChoice == "paper"):
+                computerScore++;
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. You lose. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "rock" && computerChoice == "scissors"):
+                humanScore++;
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. You win. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "paper" && computerChoice == "rock"):
+                humanScore++;
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. You win. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "paper" && computerChoice == "paper"):
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. Tie. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "paper" && computerChoice == "scissors"):
+                computerScore++;
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. You lose. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "scissors" && computerChoice == "rock"):
+                computerScore++;
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. You lose. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "scissors" && computerChoice == "paper"):
+                humanScore++;
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. You win. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            case (humanChoice == "scissors" && computerChoice == "scissors"):
+                console.log (`You picked ${humanChoice}. CPU picked ${computerChoice}. Tie. You: ${humanScore}, CPU: ${computerScore}`)
+                break;
+            default:
+                computerScore++
+                console.log (`Invalid input. CPU gets automatic point increment. You: ${humanScore}, CPU: ${computerScore}`);
         }
     }
+    playRound();
+    playRound();
+    playRound();
 }
+
+playGame();
+
+/*
+const container = document.querySelector(".container");
+const buttonRock = document.createElement("button");
+const buttonPaper = document.createElement("button");
+const buttonScissors = document.createElement("button")
+
+container.appendChild(buttonRock);
+container.appendChild(buttonPaper);
+container.appendChild(buttonScissors);
+
+buttonRock.textContent = "Rock!";
+buttonPaper.textContent = "Paper!";
+buttonScissors.textContent = "Scissors!";
+
+
+buttonPaper.addEventListener("click", () => {
+    console.log("You picked paper.");
+    let computerChoice = Math.floor(Math.random()*3)
+
+});
+*/
